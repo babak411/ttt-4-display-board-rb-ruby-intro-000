a6b905a36a94c9614650e4a9f18b05f12074ccfa
Babak Khodabandeh <babak.khodabandeh@gmail.com>
2020-03-29 12:34:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,5 @@ board = [" "," "," "," "," "," "," "," "," "]
 rows = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
 
 def display_board(board)
-  puts "#{rows[0]}, #{rows[1]}, #{rows[2]}, #{rows[3]}, #{rows[4]}"
+  puts " #{rows[0]}, #{rows[1]}, #{rows[2]}, #{rows[3]}, #{rows[4]}"
 end
